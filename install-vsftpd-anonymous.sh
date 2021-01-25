fb51ef40b19ed662d525e733f1b983fac5491ce6
loveday10000 <37687643+loveday10000@users.noreply.github.com>
2021-01-25 11:40:01 +0800

    Delete install-vsftpd-anonymous.sh

--- install-vsftpd-anonymous.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo apt-get install vsftpd
-sudo sed -i 's#anonymous_enable=NO#anonymous_enable=YES#' /etc/vsftpd.conf
-sudo service vsftpd start
-echo '————————请把文件放在/src/ftp文件夹，浏览器打开ftp://localhost即可访问—————————
-启动vsftpd方法:service vsftpd start'
